57ad78eedfa68fb8e4fba7fa55a6b6a89cffdbf1
kdragos <kristindragos@gmail.com>
2015-03-08 12:05:31 -0700

    added bower

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 $(function() {
 	$(".credentials").on("click", "h3", function(){
 		console.log('click');
-		$(this).parent('div').find('ul').toggleClass('expand');
+		$(this).parent('div').toggleClass('expand');
 
 	});
 });
